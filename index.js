7ffa94edd924994411005dbea66e991c8c91ca64
Ron Miller <rmiller232501@gmail.com>
2024-02-22 07:33:44 -0500

    Create questions and setup license choices

--- index.js
+++ index.js
@@ -1,40 +1,65 @@
 import inquirer from "inquirer";
 import fs from "fs";
 import { generateMarkdown } from "./utils/generateMarkdown.js";
+import licenses from "./utils/licenses.json" assert { type: "json" };
+
+const licenseChoices = Object.keys(licenses);
 
 const questions = [
   {
     type: "input",
     name: "title",
     message: "What is the title of your project?",
+    validate: (input) => (input ? true : "Please provide a title."),
   },
   {
     type: "input",
     name: "description",
     message: "Please provide a description of your project.",
+    validate: (input) => (input ? true : "Please provide a description."),
   },
-  //   "Description",
-  //   "Installation",
-  //   "Usage",
-  //   "License",
-  //   "Credits",
+  {
+    type: "input",
+    name: "installation",
+    message:
+      "Please provide installation instructions for your project. (Leave empty for none)",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message:
+      "Please provide usage information for your project. (Leave empty for none)",
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "Please provide credits for your project. (Leave empty for none)",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What license does your project have?",
+    choices: ["None", ...licenseChoices],
+    loop: false,
+  },
+  {
+    type: "confirm",
+    name: "hasTableOfContents",
+    message: "Would you like to include a table of contents?",
+  },
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) =>
     err ? console.error(err) : console.log("Success!")
   );
 }
 
-// TODO: Create a function to initialize app
 function init() {
   inquirer
     .prompt(questions)
     .then((answers) => {
       const markdown = generateMarkdown(answers);
-      console.log(markdown);
-
       writeToFile("README.md", markdown);
     })
     .catch((err) => console.error(err));
